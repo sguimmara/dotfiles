9abf08d30163c6219a620ed3f52909e7e526eadc
Sébastien Guimmara <5512096+sguimmara@users.noreply.github.com>
2019-05-11 13:13:36 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -16,18 +16,3 @@ source $OMF_PATH/init.fish
 
 #Load device specific configuration, if any
 source ~/.config/fish/custom.fish
-
-#set LIBRARIES "$HOME/libraries"
-
-#set PATH "$HOME/.local/bin" $PATH
-#set PATH "$HOME/tools/cmake-3.12.1-Linux-x86_64/bin" $PATH
-
-# custom tools
-#set PATH "$LIBRARIES/glfw/.build/lib" $PATH
-
-# Vulkan SDK
-#set VULKANROOT "$LIBRARIES/vulkansdk/1.1.92.1"
-#set PATH "$VULKANROOT/x86_64/bin" $PATH
-#set -g -x VULKAN_SDK "$VULKANROOT/x86_64"
-#set -g -x VK_LAYER_PATH "$VULKANROOT/x86_64/etc/explicit_layer.d"
-#set -g -x LD_LIBRARY_PATH "$VULKANROOT/x86_64/lib"
